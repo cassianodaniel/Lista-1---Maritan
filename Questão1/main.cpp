499b00fb85b6344cfdf05712f012a50a1ebe84fe
Daniel Chaves <52015731+danielcassiano@users.noreply.github.com>
2019-12-05 10:04:34 -0600

    Add files via upload

---
+++ Questão1/main.cpp
@@ -0,0 +1,16 @@
+1) Sobre o Paradigma de Programação Orientada a objetos, responda as seguintes questões:
+(a) O que são classes e objetos? Explique a diferença entre os dois.
+    R: As classes servem para definir estrutura e comportamento.
+    Os objetos são como filhos das classes que possuem suas estruturas e comportamentos para estes.
+
+(b) Para que serve um construtor e quando e como ele pode ser utilizado?
+    R:Um construtor serve para construir ou inicializar determinadas estruturas e comportamentos no programa.
+    De acordo com as especificações do programa, novos construtores podem ser inicializados ou construídos.
+
+(c) Quais são os benefícios do encapsulamento? Exemplifique.
+     O encapsulamento esconde detalhes de implementação do objeto (métodos) e o que sobra visível é a sua interface, isto é, o conjunto de todas as mensagens a que ele pode responder.
+     Uma vez que o objeto é encapsulado, seus detalhes de implementação não são mais imediatamente acessíveis.
+     Ao invés disso, eles são empacotados e são somente indiretamente acessíveis através da interface do objeto.
+     A única forma de acessar a um objeto encapsulado é através da troca de mensagens: é enviada uma mensagem ao objeto, o objeto mesmo seleciona o método pelo qual ele irá reagir à mensagem.
+
+     Ou seja, o encapsulamento oculta as informações privadas e as modifica sem que estas sejam explícitas.
